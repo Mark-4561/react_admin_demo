7d5fe1d55aa3d008f8de1e72e87aeebbe3c00143
Mark-4561 <74191452+Mark-4561@users.noreply.github.com>
2022-03-01 11:06:23 +0800

    更新node_modules

--- server/models/RoleModel.js
+++ server/models/RoleModel.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
 能操作roles集合数据的Model
  */
@@ -19,25 +18,3 @@ const RoleModel = mongoose.model('roles', roleSchema)
 
 // 4. 向外暴露Model
 module.exports = RoleModel
-=======
-/*
-能操作roles集合数据的Model
- */
-// 1.引入mongoose
-const mongoose = require('mongoose')
-
-// 2.字义Schema(描述文档结构)
-const roleSchema = new mongoose.Schema({
-  name: {type: String, required: true}, // 角色名称
-  auth_name: String, // 授权人
-  auth_time: Number, // 授权时间
-  create_time: {type: Number, default: Date.now}, // 创建时间
-  menus: Array // 所有有权限操作的菜单path的数组
-})
-
-// 3. 定义Model(与集合对应, 可以操作集合)
-const RoleModel = mongoose.model('roles', roleSchema)
-
-// 4. 向外暴露Model
-module.exports = RoleModel
->>>>>>> 17a724a2557ff3f320c71128f240ed4f30186d8e
--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
 能操作users集合数据的Model
  */
@@ -30,37 +29,4 @@ UserModel.findOne({username: 'admin'}).then(user => {
 })
 
 // 4. 向外暴露Model
-=======
-/*
-能操作users集合数据的Model
- */
-// 1.引入mongoose
-const mongoose = require('mongoose')
-const md5 = require('blueimp-md5')
-
-// 2.字义Schema(描述文档结构)
-const userSchema = new mongoose.Schema({
-  username: {type: String, required: true}, // 用户名
-  password: {type: String, required: true}, // 密码
-  phone: String,
-  email: String,
-  create_time: {type: Number, default: Date.now},
-  role_id: String
-})
-
-// 3. 定义Model(与集合对应, 可以操作集合)
-const UserModel = mongoose.model('users', userSchema)
-
-// 初始化默认超级管理员用户: admin/admin
-UserModel.findOne({username: 'admin'}).then(user => {
-  if(!user) {
-    UserModel.create({username: 'admin', password: md5('admin')})
-            .then(user => {
-              console.log('初始化用户: 用户名: admin 密码为: admin')
-            })
-  }
-})
-
-// 4. 向外暴露Model
->>>>>>> 17a724a2557ff3f320c71128f240ed4f30186d8e
 module.exports = UserModel
